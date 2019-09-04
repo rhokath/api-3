68fca53378aafc394fbdc5def114215f723b1f3d
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-09-04 13:49:11 -0400

    aded routes for suppliers and products

--- api/server.js
+++ api/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
+const productsRouter = require('../products/products-router');
+const suppliersRouter = require('../suppliers/suppliers-router');
 const server = express();
+//global middleware 
+server.use(express.json())
+
+server.use('/products', productsRouter);
+server.use('/suppliers', suppliersRouter);
+
+//route handlers
 
 server.get('/', (req, res)=> {
     res.status(200).json({api: 'working'});
---
+++ products/products-router.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const router = express.Router();
+router.use(express.json());
+
+router.get('/', (req, res)=> {
+    res.send('get to /products/')
+})
+router.get('/:id', (req, res)=> {
+    const {id} = req.params;
+    res.send(`get to /products/${id}`)
+})
+
+
+module.exports = router;
